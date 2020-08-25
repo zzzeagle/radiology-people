b820d1e37d2f0c7abeae4fac76b531bc8999f614
Zachary Eagle <zachary.eagle@gmail.com>
2020-08-25 08:50:34 -0700

    Generalizing plugin to be used for all people pages

--- SOMRADListResidents/ResidentProfiles.php
+++ SOMRADListResidents/ResidentProfiles.php
@@ -1,52 +1,50 @@
 <?php
 /**
-* Plugin Name: SOMRAD - List Residents
-* Description: This plugin will be used to list residents
+* Plugin Name: SOMRAD - List People
+* Description: This plugin will be used to list People
 * Version: 0.0.1
 * Author: Zachary Eagle
 */
-function noHTML($input, $encoding = 'UTF-8')
-{
-	return htmlentities($input, ENT_QUOTES | ENT_HTML5, $encoding);
-}
-	
-function add_level_vars_filter( $vars ){
-  $vars[] = "level";
-  return $vars;
-}
-add_filter( 'query_vars', 'add_level_vars_filter' );
 
-function list_residents( $atts ){
+
+function list_rad_people( $atts ){
 	$a = shortcode_atts( array(
 		'classification' => '',
+		'section' => '',
+		'fields' => '',
+		'labels' => 'true',
 	), $atts );
 	
-
-	$level = noHTML(get_query_var( 'level'));
-	$level = ucwords(str_replace('%20', ' ', $level));
-	$levelClass = "Resident - " . $level;
 	
 $out .= '<table>';
 
 $args = array(
 	'numberposts'	=> -1,
 	'post_type'	=> 'person',
-	'meta_query' => array(
-				'relation' => 'AND',	
+	'meta_query' =>array(
+				'relation' => 'AND',
 				array(
 					'key' => 'classification',
-					'value' => 'Resident',
+					'value' => $a['classification'],
 					'compare' => 'LIKE',
 			   ),
-			   	array(
-					'key' => 'classification',
-					'value' => $levelClass,
+			   array(
+					'key' => 'section',
+					'value' => $a['section'],
 					'compare' => 'LIKE',
-				),
-				),
-	'orderby'        => 'meta_value',
-	'meta_key'       => 'last_name', 
-	'order'          => 'ASC',
+			   ),
+			   'section_chief' => array(
+					'key' => 'section_chief',
+					'compare' => 'exists',
+					),
+				'last_name' => array(
+					'key' => 'last_name',
+					'compare' => 'exists',
+					),
+			),
+	'orderby' =>  array('section_chief' => 'DESC',
+						'last_name' => 'ASC',),
+
 );
 
 
@@ -56,31 +54,44 @@ $the_query;
 ?>
 <?php if( $the_query->have_posts() ):
 	$i = 0;
-	if($level):
-		$out .= '<h1>' . $level . ' - Residents</h1>';
-	else:
-		$out .= '<h1>All Residents</h1>';
-	endif;
 	$out .= '<ul class="flex-container">';
 	while( $the_query->have_posts() ) : $the_query->the_post();
-					$out .= '<li class="flex-item">';
-					$out .= '<div class="person-flex">';
-					$out .= '<div style="clear:right;"><img style="max-height:200px;display:block;margin:auto;" src="'.get_field( 'picture' ).'""></div>';
-					$out .= '<div class="person-text">';
+					if($i==0 and get_field('section_chief')==1):
+						$sc = 1;
+						$sectionchiefstyle = '-section-chief';
+					else:
+						$sc = 0;
+						$sectionchiefstyle = '';
+					endif;
+					$out .= '<li class="flex-item'. $sectionchiefstyle. '">';
+					$out .= '<div class="person-flex'. $sectionchiefstyle. '">';
+					$out .= '<div style="clear:right;"><img class="person-image" style="" src="'.get_field( 'picture' ).'""></div>';
+					$out .= '<div class="person-text'. $sectionchiefstyle. '">';
 					$out .= '<h3 style="margin-top:5px;margin-bottom:0px;">'.get_field( 'first_name' ).' '.get_field( 'last_name' ).', '.get_field( 'suffix' ).'</h3>';
 					$out .= '<p style="overflow:hidden;">';
-					$out .= get_field('program').'<br>';
-					$fields = ["medical_school","undergraduate_school"];
+					$fields = explode(",", $a['fields']);
 					foreach ($fields as $field){
 						$item = get_field_object($field);
 						if($item['value']):
-							$out .= '<b>'.$item['label'].': </b>' . $item['value'] . '<br>';
+							$value = $item['value'];
+							if(is_array($value)):
+								$value = implode(", ",  $value);
+							endif;
+							if($a['labels'] == 'false'):
+								$out .= $value . '<br>';
+							else:
+								$out .= '<b>'.$item['label'].': </b>' . $value . '<br>';
+							endif;
 						endif;
 					}
 					$buttonshortcode = do_shortcode('[button color=purple type=small=true url="'.get_permalink().'"]View Full Profile[/button]');
-					
+					if($sc==1):
+						$out .= '<div class="person-more">'. $buttonshortcode . '</div>';
+					endif;
 					$out .= '<br></p></div>';
-					$out .= '<div class="person-more">'. $buttonshortcode . '</div>';
+					if($sc==0):
+						$out .= '<div class="person-more">'. $buttonshortcode . '</div>';
+					endif;
 					$out .= '</div>';
 					$out .= '</div>';
 
@@ -95,7 +106,7 @@ $out .= "</table>";
 return $out;
 }
 
-add_shortcode( 'list-residents', 'list_residents');
+add_shortcode( 'list-rad-people', 'list_rad_people');
 
 function person_flex_style() {
     wp_enqueue_style( 'style-name', plugins_url('person-flex.css',__FILE__ ));
