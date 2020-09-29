b7aaeaf08b7c3448132941a7ff17a692a34907b1
O Lim <oudahm.lim@outlook.com>
2020-09-29 16:00:28 -0700

    wahh so much learned

--- archive-person.php
+++ archive-person.php
@@ -6,13 +6,13 @@
 
     <div class="col-md-12 uw-content" role='main'>
 
-    	<h2 class="uw-site-title">Meet Our Faculty</h2>
+    	<h2 class="uw-site-title">Meet THE Faculty</h2>
     	
     	 <?php if (is_front_page()) { get_template_part( 'menu', 'mobile' ); }?>
 
       <div id='main_content' class="uw-body-copy" tabindex="-1">
       <div class="col-md-12">
-      	<h2>Meet Our Faculty</h2>
+      	<h2>Meet The Faculty</h2>
       	<div class="col-md-3">
       	<ul>
       	<li><a href="/radiology-personnel">All</a></li>
@@ -23,7 +23,7 @@
       		<ul>
       			<li><a href="./?clinical_section=abdominal%20imaging">Abdominal Imaging</a></li>
       			<li><a href="./?clinical_section=breast%20imaging">Breast Imaging</a></li>
-				<li><a href="./?clinical_section=Cardiothoracic%20radiology">Cardiothoracic Radiology</a></li>
+				<li><a href="./?clinical_section=Cardiothoracic%20imaging">Cardiothoracic Imaging</a></li>
       			<li><a href="./?clinical_section=diagnostic%20physics">Diagnostic Physics</a></li>
       			<li><a href="./?clinical_section=emergency%20radiology">Emergency Radiology</a></li>
       			<li><a href="./?clinical_section=interventional%20radiology">Interventional Radiology</a></li>
